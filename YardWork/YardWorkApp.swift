f8dabf1a2ec5326a15555ce6761093e3d72bbf42
esplusnin <esplusnin94@yandex.ru>
2025-11-24 17:39:15 +0300

    Initial Commit

---
+++ YardWork/YardWorkApp.swift
@@ -0,0 +1,17 @@
+//
+//  YardWorkApp.swift
+//  YardWork
+//
+//  Created by Евгений on 24.11.2025.
+//
+
+import SwiftUI
+
+@main
+struct YardWorkApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
